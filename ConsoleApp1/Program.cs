070886b0f452830f3efc2a909f17b9a4aa15ab27
Tugsbayar Galbadrakh <tugs1ga@gmail.com>
2025-08-27 10:48:14 +0200

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿string name;
+string gammal;
+string du;
+
+Console.WriteLine("ditt namn");
+name = Console.ReadLine();
+Console.WriteLine("Hur gammal är du? ");
+gammal = Console.ReadLine();
+Console.WriteLine("varför skriver du de");
+du = Console.ReadLine();
+Console.WriteLine($"Så du heter {name} och {gammal} år, varför skriv du {du}");
+Console.ReadLine();
